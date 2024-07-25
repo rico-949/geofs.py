3573d7c76449f223d8dd8d19cd28e7f1137c7ede
rico-949 <157065676+rico-949@users.noreply.github.com>
2024-07-25 18:25:52 -0300

    __init__.py done

---
+++ geofs/__init__.py
@@ -0,0 +1,22 @@
+"""
+GeoFS API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic API wrapper for the GeoFS API.
+
+:copyright: (c) 2024 rico-949
+:license: MIT, see LICENSE for more details.
+"""
+
+
+__title__ = "geofs"
+__author__ = "rico-949"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024 rico-949"
+__version__ = "1.0.0"
+
+from .endpoints import *
+from .models import *
+from .client import *
+
+
