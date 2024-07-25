435655329c471636a8444d1edefcab17b005bbec
rico-949 <157065676+rico-949@users.noreply.github.com>
2024-07-25 20:49:45 -0300

    Standard JSON bodies for HTPP requests

---
+++ geofs/body.py
@@ -0,0 +1,17 @@
+map_body = {
+    "id": "",
+    "gid": None
+}
+update_body = {
+    "origin": "https://www.geo-fs.com",
+    "acid": "",
+    "sid": "",
+    "id": "",
+    "ac": "1",
+    "co": [],
+    "ve": [],
+    "st": {"gr":True,"as":0},
+    "ti": None,
+    "m": "",
+    "ci": 0
+}
