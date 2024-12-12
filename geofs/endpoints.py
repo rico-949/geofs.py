db50bd44219f7b19109f5063fd2b176731b6d128
rico-949 <157065676+rico-949@users.noreply.github.com>
2024-12-12 17:07:31 -0300

    Update endpoints.py

--- geofs/endpoints.py
+++ geofs/endpoints.py
@@ -2,6 +2,6 @@ MPS_URL = "https://mps.geo-fs.com/"
 map_endpoint = f"{MPS_URL}map"
 chat_endpoint = f"{MPS_URL}update"
 
-DATA_URL = "https://data2.geo-fs.com/"
-osm_endpoint = f"{DATA_URL}osm"
+#DATA_URL = "https://data2.geo-fs.com/"
+#osm_endpoint = f"{DATA_URL}osm"
 #satellite_endpoint = f"{DATA_URL}sent"
