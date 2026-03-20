dbf7d697ab20071ece5492f7517ecf1c00a76d10
rico-949 <157065676+rico-949@users.noreply.github.com>
2026-03-20 08:59:08 -0300

    CENSORED BY THE GMRP ADMINISTRATION

--- geofs/endpoints.py
+++ geofs/endpoints.py
@@ -29,7 +29,7 @@ SOFTWARE.
 
 MPS_URL = "https://mps.geo-fs.com/"
 map_endpoint = f"{MPS_URL}map"
-update_endpoint = f"{MPS_URL}update?l=23"
+CENSORED_endpoint = ["CENSORED BY THE GMRP ADMINISTRATION"]
 
 #DATA_URL = "https://data2.geo-fs.com/"
 #osm_endpoint = f"{DATA_URL}osm"
